ea7d0c370e6387197c9c584838ec3945058b48c6
congtrieu <ittrjeu@gmail.com>
2019-01-27 16:28:35 +0700

    clear git cache

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -d -p 8090:80 --rm --name php71-apache -v $(pwd)/app/:/var/www/webapp -v $(pwd)/vhost.conf:/etc/apache2/sites-available/000-default.conf -v $(pwd)/php.ini:/etc/php/7.1/apache2/php.ini php71-apache
+sudo docker run -d -p 8090:80 --rm --name php71-apache --link some-mysql:mysql -v $(pwd)/app/:/var/www/webapp -v $(pwd)/vhost.conf:/etc/apache2/sites-available/000-default.conf -v $(pwd)/php.ini:/etc/php/7.1/apache2/php.ini php71-apache
 
